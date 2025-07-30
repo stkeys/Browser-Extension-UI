36f5fd01dcd123b6ef58566bfa25835c47429807
Ola-olukotun Mary <maryomowumi76@gmail.com>
2025-07-30 17:20:36 +0100

    refactor: line format

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,7 @@ export default function Home() {
     <ThemeProvider>
       <div className="h-auto  bg-[linear-gradient(180deg,_#EBF2FC_0%,_#EEF8F9_100%)]  min-h-screen text-black flex flex-col items-center justify-center dark:bg-gradient-to-b dark:from-[#040918] dark:to-[#091540] dark:text-white">
         <p>Click the button to toggle dark mode!</p>
-        <button className="bg-gray-200 p-2 rounded-lg hover:bg-gray-300    transition-colors mt-2">
+        <button className="bg-gray-200 p-2 rounded-lg hover:bg-gray-300 transition-colors mt-2">
          <DarkModeToggle/>
        </button>
      </div>
