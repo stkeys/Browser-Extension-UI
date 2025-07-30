5126548ae9f8650c3672f513ac8870fc4a4e7f9d
Ola-olukotun Mary <maryomowumi76@gmail.com>
2025-07-30 17:26:21 +0100

    chore: remove some lines

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,6 @@ export default function Home() {
   <>
     <ThemeProvider>
       <div className="h-auto  bg-[linear-gradient(180deg,_#EBF2FC_0%,_#EEF8F9_100%)]  min-h-screen text-black   dark:bg-gradient-to-b dark:from-[#040918] dark:to-[#091540] dark:text-white">
-        <p>Click the button to toggle dark mode!</p>
         <button className="bg-gray-200 p-2 rounded-lg hover:bg-gray-300 transition-colors mt-2">
          <DarkModeToggle/>
        </button>
