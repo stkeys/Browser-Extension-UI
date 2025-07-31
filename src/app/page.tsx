4a417f080444d5d8a30676b764d6a184d07f3851
Ola-olukotun Mary <maryomowumi76@gmail.com>
2025-07-31 09:34:11 +0100

    feat: add features

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,15 +6,13 @@ export default function Home() {
   return (
   <>
     <ThemeProvider>
-      <div className="h-auto  bg-[linear-gradient(180deg,_#EBF2FC_0%,_#EEF8F9_100%)] min-h-screen text-black   dark:bg-gradient-to-b dark:from-[#040918] dark:to-[#091540] dark:text-white">
-        <div className="mx-[20%]"> 
-          <div className="bg-[hsl(225,23%,24%)] rounded-lg mt-10">
+      <div className=" bg-[linear-gradient(180deg,_#EBF2FC_0%,_#EEF8F9_100%)] min-h-screen overflow-hidden text-black   dark:bg-gradient-to-b dark:from-[#040918] dark:to-[#091540] dark:text-white">
+          <div className=" mx-[20%] bg-[hsl(225,23%,24%)] rounded-lg mt-10">
             <button className="bg-gray-200 p-2 rounded-lg hover:bg-gray-300 transition-colors mt-2">
               <DarkModeToggle/>
             </button>     
           </div>
          
-        </div>
         
      </div>
     </ThemeProvider>
