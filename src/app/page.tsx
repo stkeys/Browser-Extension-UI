cf9e6bf182cc3fda4defeb849808ccb11076892c
Ola-olukotun Mary <maryomowumi76@gmail.com>
2025-07-31 21:51:24 +0100

    feat: add new tag & adjust parent padding

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,8 +8,8 @@ export default function Home() {
   return (
   <>
     <ThemeProvider>
-      <div className=" bg-[linear-gradient(180deg,_#EBF2FC_0%,_#EEF8F9_100%)] min-h-screen overflow-hidden text-black   dark:bg-gradient-to-b dark:from-[#040918] dark:to-[#091540] dark:text-white">
-          <div className=" mx-[10%] bg-[hsl(200,60%,99%)] dark:bg-gradient-to-b dark:bg-[hsl(226,25%,17%)] rounded-xl mt-10 py-2 px-4 flex justify-between items-center">
+      <div className=" bg-[linear-gradient(180deg,_#EBF2FC_0%,_#EEF8F9_100%)] min-h-screen overflow-hidden text-black   dark:bg-gradient-to-b dark:from-[#040918] dark:to-[#091540] dark:text-white px-[10%]">
+          <div className="  bg-[hsl(200,60%,99%)] dark:bg-gradient-to-b dark:bg-[hsl(226,25%,17%)] rounded-xl my-10 py-2 px-4 flex justify-between items-center">
             <div>
               <Image 
                 src={Logo}
@@ -20,13 +20,15 @@ export default function Home() {
             </div>
             <button className="bg-[hsl(226,11%,37%)] p-2 rounded-xl transition-colors mt-2 flex items-center">
               <DarkModeToggle/>
-            </button>     
+            </button>
             
           </div>
          
-        
-     </div>
-    </ThemeProvider>
-  </>
+         <div>
+            <p>Extension Lists</p>
+          </div>
+        </div>
+      </ThemeProvider>
+    </>
   );
 }
