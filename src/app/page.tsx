397c569746dfe82ad6191f37cdedb9269d0b611e
Ola-olukotun Mary <maryomowumi76@gmail.com>
2025-07-31 14:19:00 +0100

    feat:add logo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,27 @@
 'use client';
+import Image from "next/image";
 import DarkModeToggle from "../../components/DarkModeToggle";
 import { ThemeProvider } from "./context/ThemeContext";
+import  Logo  from "../../public/images/logo.svg"
 
 export default function Home() {
   return (
   <>
     <ThemeProvider>
       <div className=" bg-[linear-gradient(180deg,_#EBF2FC_0%,_#EEF8F9_100%)] min-h-screen overflow-hidden text-black   dark:bg-gradient-to-b dark:from-[#040918] dark:to-[#091540] dark:text-white">
-          <div className=" mx-[10%] bg-[hsl(200,60%,99%)] dark:bg-gradient-to-b dark:bg-[hsl(225,23%,24%)] rounded-lg mt-10">
-            <button className="bg-[hsl(226,11%,37%)] p-2 rounded-lg hover:bg-gray-300 transition-colors mt-2 flex items-center">
+          <div className=" mx-[10%] bg-[hsl(200,60%,99%)] dark:bg-gradient-to-b dark:bg-[hsl(226,25%,17%)] rounded-xl mt-10 p-2 flex justify-between items-center">
+            <div>
+              <Image 
+                src={Logo}
+                alt="Logo"
+                width={150}
+                height={150}
+              />
+            </div>
+            <button className="bg-[hsl(226,11%,37%)] p-2 rounded-lg transition-colors mt-2 flex items-center">
               <DarkModeToggle/>
             </button>     
+            
           </div>
          
         
