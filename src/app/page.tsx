2bab46a4b692d497a6210ce11155a0bbe5b3e5b0
Ola-olukotun Mary <maryomowumi76@gmail.com>
2025-07-31 12:49:28 +0100

    style: background color

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
   <>
     <ThemeProvider>
       <div className=" bg-[linear-gradient(180deg,_#EBF2FC_0%,_#EEF8F9_100%)] min-h-screen overflow-hidden text-black   dark:bg-gradient-to-b dark:from-[#040918] dark:to-[#091540] dark:text-white">
-          <div className=" mx-[20%] bg-[hsl(225,23%,24%)] rounded-lg mt-10">
+          <div className=" mx-[10%] bg-[hsl(200,60%,99%)] dark:bg-gradient-to-b dark:bg-[hsl(225,23%,24%)] rounded-lg mt-10">
             <button className="bg-gray-200 p-2 rounded-lg hover:bg-gray-300 transition-colors mt-2">
               <DarkModeToggle/>
             </button>     
