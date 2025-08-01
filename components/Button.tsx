365e632f4c853a29992f82e44da134299fef2018
Ola-olukotun Mary <maryomowumi76@gmail.com>
2025-08-02 00:19:15 +0100

    feat: add background color for active buttons

--- components/Button.tsx
+++ components/Button.tsx
@@ -15,8 +15,12 @@ export default function Buttons() {
         <button
           key={index}
           onClick={() => setActiveButton(index)}
-          className={`p-[12px] btn-bg rounded-2xl transition-colors mb-4
-          ${activeButton === index ? 'bg-[hsl(hsl(3,77%,44%)] text-white' : 'bg-[hsl(hsl(3,77%,44%))] dark:bg-[hsl(226,11%,37%)] text-black dark:text-white'}`}>
+          className={
+          `p-[12px]  rounded-2xl transition-colors mb-4
+          ${activeButton === index 
+          ? 'bg-[hsl(3,77%,44%)] text-white dark:bg-[hsl(3,71%,56%)] dark:text-black' 
+          : 'bg-[hsl(200,60%,99%)] dark:bg-[hsl(226,11%,37%)] '}
+          `}>
           {btn.label}
         </button>
       ))}
