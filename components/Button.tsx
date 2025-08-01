e994ddf0cd24ba1619a0340c9f47902c04f63b48
Ola-olukotun Mary <maryomowumi76@gmail.com>
2025-08-01 12:18:14 +0100

    feat: add path for button

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,14 +1,18 @@
 'use client';
 
 export default function Buttons() {
-  const buttons = ['All', 'Active', 'Inactive'];
+  const buttons = [
+    { label: 'All', path: '/'},
+    { label: 'Active', path: '/'},
+    { label: 'Inactive', path: '/'},
+    ]
   return (
     <div className="space-x-2">
-      {buttons.map((button, index) => (
+      {buttons.map((btn, index) => (
         <button
           key={index}
-           className="p-[12px] btn-bg rounded-2xl transition-colors mb-4">
-            {button}
+          className="p-[12px] btn-bg rounded-2xl transition-colors mb-4">
+          {btn.label}
         </button>
       ))}
     </div>
