75dfcd8e2c510473ab5879f00220e604d47edc77
Ola-olukotun Mary <maryomowumi76@gmail.com>
2025-08-02 13:36:11 +0100

    style: add shadow-md to white background nav area

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,13 +4,14 @@ import DarkModeToggle from "../../components/DarkModeToggle";
 import { ThemeProvider } from "./context/ThemeContext";
 import  Logo  from "../../public/images/logo.svg"
 import Buttons from "../../components/Button";
+import FlexSwitcher from "../../components/Flex-Switcher";
 
 export default function Home() {
   return (
   <>
     <ThemeProvider>
       <div className="font-sans bg-[linear-gradient(180deg,_#EBF2FC_0%,_#EEF8F9_100%)] min-h-screen overflow-hidden text-black   dark:bg-gradient-to-b dark:from-[#040918] dark:to-[#091540] dark:text-white px-[10%]">
-          <div className="  bg-[hsl(200,60%,99%)] dark:bg-gradient-to-b dark:bg-[hsl(226,25%,17%)] rounded-xl my-10 py-2 px-4 flex justify-between items-center">
+          <div className=" shadow-md bg-[hsl(200,60%,99%)] dark:bg-gradient-to-b dark:bg-[hsl(226,25%,17%)] rounded-xl my-10 py-2 px-4 flex justify-between items-center">
             <div>
               <Image 
                 src={Logo}
@@ -29,6 +30,9 @@ export default function Home() {
             <p className="text-[2rem] mb-4 font-bold text-[hsl(227,75%,14%)] dark:text-white">Extensions List</p>
             <Buttons />
           </div>
+          <FlexSwitcher>
+           
+          </FlexSwitcher>
         </div>
       </ThemeProvider>
     </>
