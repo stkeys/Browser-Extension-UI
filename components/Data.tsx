4ec651006f52c3fc03dbb6f922c5283dcb6cec16
Ola-olukotun Mary <maryomowumi76@gmail.com>
2025-08-18 17:30:30 +0100

    chore: remove boolean in data

--- components/Data.tsx
+++ components/Data.tsx
@@ -3,56 +3,56 @@ export const Data = [
     image: "/images/logo-devlens.svg",
     title: "DevLens",
     description: "Quickly inspect page layouts and visualize element boundaries",
-    switch: false,
+   
   },
 
   {
     image: "/images/logo-style-spy.svg",
     title: "StyleSpy",
     description: "Quickly inspect page layouts and visualize element boundaries",
-    switch: false,
+   
   },
 
   {
     image: "/images/logo-speed-boost.svg",
     title: "JSONWizard",
     description: "Formats, validates, and prettifies JSON responses in-browser",
-    switch: false,
+    
   },
 
   {
     image: "/images/logo-tab-master-pro.svg",
     title: "TabMaster Pro",
     description: "Organizes browser tabs into groups and sessions",
-    switch: false,
+    
   },
 
   {
     image: "/images/logo-speed-boost.svg",
     title: "SpeedBoost",
     description: "Optimizes browser resource usage to accelerate page loading",
-    switch: false,
+    
   },
 
   {
     image: "/images/logo-viewport-buddy.svg",
     title: "ViewportBuddy",
     description: "Simulates various screen  resolutions directly through the browser",
-    switch: false,
+    
   },
 
   {
     image: "/images/logo-markup-notes.svg",
     title: "Markup Notes",
     description: "Easily annotate and highlight web pages with notes",
-    switch: false,
+    
   },
   
   {
     image: "/images/logo-grid-guides.svg",
     title: "GridGuides",
     description: "Overlay customizable grid layouts on web pages",
-    switch: false,
+    
   },
 
 
