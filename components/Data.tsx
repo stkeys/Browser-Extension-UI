9a59dbd5298edef8173df9b01581d1f728396a90
Ola-olukotun Mary <maryomowumi76@gmail.com>
2025-08-23 14:57:42 +0100

    chore: line format

--- components/Data.tsx
+++ components/Data.tsx
@@ -3,56 +3,48 @@ export const Data = [
     image: "/images/logo-devlens.svg",
     title: "DevLens",
     description: "Quickly inspect page layouts and visualize element boundaries",
-   
   },
 
   {
     image: "/images/logo-style-spy.svg",
     title: "StyleSpy",
     description: "Quickly inspect page layouts and visualize element boundaries",
-   
   },
 
   {
     image: "/images/logo-speed-boost.svg",
     title: "JSONWizard",
-    description: "Formats, validates, and prettifies JSON responses in-browser",
-    
+    description: "Formats, validates, and prettifies JSON responses in-browser", 
   },
 
   {
     image: "/images/logo-tab-master-pro.svg",
     title: "TabMaster Pro",
     description: "Organizes browser tabs into groups and sessions",
-    
   },
 
   {
     image: "/images/logo-speed-boost.svg",
     title: "SpeedBoost",
     description: "Optimizes browser resource usage to accelerate page loading",
-    
   },
 
   {
     image: "/images/logo-viewport-buddy.svg",
     title: "ViewportBuddy",
     description: "Simulates various screen  resolutions directly through the browser",
-    
   },
 
   {
     image: "/images/logo-markup-notes.svg",
     title: "Markup Notes",
     description: "Easily annotate and highlight web pages with notes",
-    
   },
   
   {
     image: "/images/logo-grid-guides.svg",
     title: "GridGuides",
     description: "Overlay customizable grid layouts on web pages",
-    
   },
 
 
